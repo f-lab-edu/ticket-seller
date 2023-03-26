aaa186777ac4be02a94363cae26769c246b6fa03
jaejeong1 <wowjd555@gmail.com>
2023-03-27 02:24:28 +0900

    MemberServiceTest 내부 검증 및 테스트 로직 추가

--- src/test/java/com/jj/ticketseller/service/MemberServiceTest.java
+++ src/test/java/com/jj/ticketseller/service/MemberServiceTest.java
@@ -2,15 +2,19 @@ package com.jj.ticketseller.service;
 
 import com.jj.ticketseller.domain.Member;
 
-import com.jj.ticketseller.domain.MemberFactory;
 import com.jj.ticketseller.dto.MemberDTO;
+import com.jj.ticketseller.repository.MemberCustomRepository;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.junit.runner.RunWith;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MemberServiceTest {
@@ -18,34 +22,66 @@ public class MemberServiceTest {
     private final String city = "city";
     private final String street = "street";
     private final String zipcode = "zipcode";
-    @Mock
+    @InjectMocks
     private MemberService memberService;
+    @Mock
+    private MemberCustomRepository memberRepository;
 
     @Test
     public void join() throws IllegalStateException {
+        // given
         MemberDTO memberDTO = new MemberDTO(name, city, street, zipcode);
-        Member member = MemberFactory.createMember(memberDTO);
-
-        Mockito.when(memberService.join(memberDTO))
-                .thenReturn(member.getId());
-        Mockito.when(memberService.findOne(member.getId())).thenReturn(member);
-
-        Long savedId = memberService.join(memberDTO);
-
-        assertEquals(member, memberService.findOne(savedId));
+        Member member = new Member(name);
+        member.setId(1L);
+        when(memberRepository.findByName(name)).thenReturn(Collections.emptyList());
+        doAnswer(invocation -> {
+            Member argMember = invocation.getArgument(0);
+            argMember.setId(1L);
+            return null;
+        }).when(memberRepository).save(any(Member.class));
+        // when
+        long memberId = memberService.join(memberDTO);
+        // then
+        assertEquals(1L, memberId);
+        verify(memberRepository).findByName(name);
+        verify(memberRepository).save(any(Member.class));
     }
 
-    @Test(expected = IllegalStateException.class)
-    public void duplicateMemberException() throws IllegalStateException {
+    @Test
+    public void duplicateMemberException() {
+        // given
         MemberDTO memberDTO = new MemberDTO(name, city, street, zipcode);
-        Member member = MemberFactory.createMember(memberDTO);
+        Member member = new Member(name);
+        when(memberRepository.findByName(member.getName())).thenReturn(Collections.singletonList(member));
+        // when & then
+        IllegalStateException exception = assertThrows(IllegalStateException.class, () -> memberService.join(memberDTO));
+        assertEquals("이미 존재하는 회원입니다.", exception.getMessage());
+        verify(memberRepository).findByName(member.getName());
+    }
 
-        Mockito.when(memberService.join(memberDTO)).thenReturn(member.getId());
-        Mockito.when(memberService.join(memberDTO)).thenThrow(IllegalStateException.class);
+    @Test
+    public void testFindMembers() {
+        // given
+        Member member = new Member(name);
+        when(memberRepository.findAll()).thenReturn(Collections.singletonList(member));
+        // when
+        List<Member> members = memberService.findMembers();
+        // then
+        assertEquals(Collections.singletonList(member), members);
+        verify(memberRepository).findAll();
+    }
 
-        memberService.join(memberDTO);
-        memberService.join(memberDTO);
-
-        fail("예외 발생");
+    @Test
+    public void testFindOne() {
+        // given
+        Long id = 1L;
+        Member member = new Member(name);
+        member.setId(id);
+        when(memberRepository.findOne(id)).thenReturn(member);
+        // when
+        Member foundMember = memberService.findOne(id);
+        // then
+        assertEquals(member, foundMember);
+        verify(memberRepository).findOne(id);
     }
 }
